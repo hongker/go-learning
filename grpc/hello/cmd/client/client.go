f68a51f40b057b7d67c7141263200a6ab5c94213
colincchen <colincchen@tencent.com>
2023-06-05 22:12:55 +0800

    更新grpc

---
+++ grpc/hello/cmd/client/client.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"demo/grpc/hello/pb"
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/balancer/roundrobin"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+)
+
+func main() {
+	// 与服务器建立连接
+	cc, err := grpc.Dial(":8080",
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithDefaultServiceConfig(fmt.Sprintf(`{"LoadBalancingPolicy": "%s"}`, roundrobin.Name)),
+	)
+	if err != nil {
+		log.Fatalf("failed to connect server: %v", err)
+	}
+
+	client := pb.NewGreeterClient(cc)
+	req := &pb.HelloRequest{Name: "alice"}
+
+	// 发送请求
+	resp, err := client.SayHello(context.Background(), req)
+	if err != nil {
+		log.Fatalf("failed to say hello: %v", err)
+	}
+
+	log.Println("success receive response: ", resp.Message)
+}
---
+++ grpc/hello/cmd/server/server.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"demo/grpc/hello/pb"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	// 监听8080段口
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer(
+		grpc.ChainUnaryInterceptor(interceptor),
+	)
+	pb.RegisterGreeterServer(grpcServer, NewServer())
+	grpcServer.Serve(lis)
+}
+
+type Server struct {
+	pb.UnimplementedGreeterServer
+}
+
+func (s Server) SayHello(ctx context.Context, request *pb.HelloRequest) (*pb.HelloReply, error) {
+	return &pb.HelloReply{Message: "Welcome " + request.Name}, nil
+}
+
+func NewServer() *Server {
+	return &Server{}
+}
+
+func interceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	// 在调用方法之前记录日志
+	before := time.Now()
+
+	// 调用实际的gRPC方法
+	resp, err := handler(ctx, req)
+
+	// 在调用方法之后记录日志
+	log.Printf("After Calling Method: %s, time used:%v", info.FullMethod, time.Since(before))
+
+	// 返回结果
+	return resp, err
+}
